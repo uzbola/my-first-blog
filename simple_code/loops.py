fddfaa3139ea75fe9cab248bead2dfd503687d55
Uzbola <uzbola@gmail.com>
2018-12-09 13:42:36 -0500

    initial commit

---
+++ simple_code/loops.py
@@ -0,0 +1,14 @@
+#Functions
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+	
+for i in range(1, 6):
+    print(i)
+	
+	
---
+++ simple_code/test.py
@@ -0,0 +1,13 @@
+temperature = float(input('What is the temperature? '))
+
+print ("Temperature you have entered is", temperature)
+
+if temperature > 70:
+	print('Wear shorts.')
+else:
+	print('Wear long pants.')
+	print('Get some exercise outside.')
+
+
+	
+	
